6a9326d7ff810659b4a723c1bc7bbf7f3c02f6a4
Manish <manish.katoch9@gmail.com>
2020-06-03 02:37:08 +0530

    Initial Commit

---
+++ Target Support Files/Pods-genericModelMVVM-genericModelMVVMUITests/Pods-genericModelMVVM-genericModelMVVMUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_genericModelMVVM_genericModelMVVMUITests : NSObject
+@end
+@implementation PodsDummy_Pods_genericModelMVVM_genericModelMVVMUITests
+@end
---
+++ Target Support Files/Pods-genericModelMVVM/Pods-genericModelMVVM-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_genericModelMVVM : NSObject
+@end
+@implementation PodsDummy_Pods_genericModelMVVM
+@end
